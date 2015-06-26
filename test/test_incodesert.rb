c27cdd975392ed74e37c59b3dfab1e515342f1ec
Kirk Bowers <kirkbowers@yahoo.com>
2015-06-25 22:06:25 -0700

    Added tests for trailing whitespace after token names.

--- test/test_incodesert.rb
+++ test/test_incodesert.rb
@@ -125,6 +125,54 @@ EOF
       assert_equal "", doc.warnings
     end    
     
+    should 'replace simple token with matching source with trailing whitespaceand no warnings' do
+    
+      # You can't see it, but the two tokens here in the source have varying amounts of
+      # trailing whitespace.  It should match anyway.
+      source = <<EOF
+  // <<< token 
+  
+  replaced_function();
+  
+  // >>> token   
+EOF
+
+      result = <<EOF
+  this_should_stay_the_same();
+
+  // <<< token 
+  
+  replaced_function();
+  
+  // >>> token   
+  
+  also_should_stay_same();
+
+  // <<< token with spaces
+
+  // something else to be replaced
+  
+  // >>> token with spaces
+  
+  bringing_it_home();
+EOF
+
+      extractions = <<EOF
+  // <<< token
+  
+  code_to_be_replaced();
+  
+  // >>> token
+EOF
+
+      doc = Incodesert::Documents.new(source, @destination)
+      doc.no_warn = true
+      doc.perform_insertions!
+      assert_equal result, doc.destination
+      assert_equal extractions, doc.extractions
+      assert_equal "", doc.warnings
+    end    
+    
     should 'replace simple token with matching source and with warnings' do
       source = <<EOF
   // <<< token
@@ -608,5 +656,63 @@ EOF
       assert_equal warnings, doc.warnings
     end
   end
+  
+  context 'With C-style comments and trailing whitespace' do
+    setup do
+      # You can't see it, but there is trailing whitespace at the end of each of the 
+      # tokens in this setup, and different amounts of whitespace at that.
+      # Principle of least surprise tells us that if you can't see something
+      # (unseen whitespace), it shouldn't break how you expect the software to behave.
+    
+      @destination = <<EOF
+  this_should_stay_the_same();
+
+  // <<< token with trailing whitespace      
+  
+  code_to_be_replaced();
+  
+  // >>> token with trailing whitespace  
+  
+  also_should_stay_same();
+EOF
+    end
+    
+    should 'replace token with trailing whitespace with matching source and no warnings' do
+      source = <<EOF
+  // <<< token with trailing whitespace
+  
+  replaced_function();
+  
+  // >>> token with trailing whitespace
+EOF
+      result = <<EOF
+  this_should_stay_the_same();
+
+  // <<< token with trailing whitespace
+  
+  replaced_function();
+  
+  // >>> token with trailing whitespace
+  
+  also_should_stay_same();
+EOF
+
+      extractions = <<EOF
+  // <<< token with trailing whitespace      
+  
+  code_to_be_replaced();
+  
+  // >>> token with trailing whitespace  
+EOF
+
+      doc = Incodesert::Documents.new(source, @destination)
+      doc.no_warn = true
+      doc.perform_insertions!
+      assert_equal result, doc.destination
+      assert_equal extractions, doc.extractions
+      assert_equal "", doc.warnings
+    end    
+  end
+    
 end
 
